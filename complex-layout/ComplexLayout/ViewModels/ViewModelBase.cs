4c1c440ee89eff3e2dadd1dc29ba4070377feea8
Vedran Filipovic <VedranFilipovic@hotmail.com>
2021-01-01 21:01:30 -0800

    Added complex layout sample

---
+++ complex-layout/ComplexLayout/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexLayout.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
